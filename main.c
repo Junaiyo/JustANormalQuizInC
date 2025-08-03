56b51d58893a9cd1911fad5afb6c44d7311d1cab
Junaiyo <bandidoblackpheonix@gmail.com>
2025-08-03 04:27:37 -0300

    Tenbo toque

--- main.c
+++ main.c
@@ -35,7 +35,7 @@
 #define dddd answers
 #define ddddd correct_answers
 #define dddddd CallQuiz
-#define ddddddd Num
+#define ddddddd Num 
 #define dddddddd Alt
 #define d8 i
 #define d9 line
@@ -47,73 +47,49 @@
 #define d1m 1
 #define d0m j
 
-aaa ddd bbbbbb 20 bbbbbbb bbbbbb 67 bbbbbbb b8 bbb bbb
-  "Qual desses imprime algo na tela em python?" bbbb b9 bbb
-  "Qual dessas linguagens é compilada?" bbbb b9 bbb
-  "Em JavaScript, qual palavra-chave declara uma variável?" bbbb b9 bbb
-  "Qual símbolo é usado para comentário em C++? (linha unica)" bbbb b9 bbb
-  "Em SQL, qual comando busca dados?" bbbb b9 bbb
-  "Em HTML, qual tag define um parágrafo?" bbbb b9 bbb
-  "Em CSS, como se seleciona uma classe?" bbbb b9 bbb
-  "Qual método adiciona um elemento ao final de uma lista em Python?" bbbb b9
-    bbb " Em Java, qual palavra indica uma classe?" bbbb b9 bbb
-  "Qual dessas linguagens é orientada a objetos?" bbbb b9 bbb
-  "Em PHP, qual símbolo inicia uma variável?" bbbb b9 bbb
-  "Em Ruby, qual palavra define um método?" bbbb b9 bbb
-  "Qual operador faz a multiplicação em JavaScript?" bbbb b9 bbb
-  "Em Bash, qual comando exibe o diretório atual?" bbbb b9 bbb
-  "Em C#, qual palavra-chave cria uma interface?" bbbb b9 bbb
-  "Em Swift, como declara uma constante?" bbbb b9 bbb
-  "Qual linguagem usa indentação para blocos de código?" bbbb b9 bbb
-  "Em JavaScript, o que \"===\" significa?" bbbb b9 bbb
-  "Em SQL, qual comando atualiza dados?" bbbb b9 bbb
-  "Em HTML, qual atributo define o link de um <a>?" bbbb bbbb bbbbb
+   aaa ddd bbbbbb 20 bbbbbbb bbbbbb 67 bbbbbbb b8 bbb bbb "Qual desses imprime algo na tela em python?" bbbb b9 bbb "Qual dessas linguagens é compilada?" bbbb b9 bbb "Em JavaScript, qual palavra-chave declara uma variável?" bbbb b9 bbb "Qual símbolo é usado para comentário em C++? (linha unica)" bbbb b9 bbb "Em SQL, qual comando busca dados?" bbbb b9 bbb "Em HTML, qual tag define um parágrafo?" bbbb b9 bbb "Em CSS, como se seleciona uma classe?" bbbb b9 bbb "Qual método adiciona um elemento ao final de uma lista em Python?" bbbb b9 bbb " Em Java, qual palavra indica uma classe?" bbbb b9 bbb "Qual dessas linguagens é orientada a objetos?" bbbb b9 bbb "Em PHP, qual símbolo inicia uma variável?" bbbb b9 bbb "Em Ruby, qual palavra define um método?" bbbb b9 bbb "Qual operador faz a multiplicação em JavaScript?" bbbb b9 bbb "Em Bash, qual comando exibe o diretório atual?" bbbb b9 bbb "Em C#, qual palavra-chave cria uma interface?" bbbb b9 bbb "Em Swift, como declara uma constante?" bbbb b9 bbb "Qual linguagem usa indentação para blocos de código?" bbbb b9 bbb "Em JavaScript, o que \"===\" significa?" bbbb b9 bbb "Em SQL, qual comando atualiza dados?" bbbb b9 bbb "Em HTML, qual atributo define o link de um <a>?" bbbb bbbb bbbbb
 
-    aaa dddddddd bbbbbb 4 bbbbbbb b8 bbb 'a' b9 'b' b9 'c' b9 'd' bbbb bbbbb
+  aaa dddddddd bbbbbb 4 bbbbbbb b8 bbb 'a' b9 'b' b9 'c' b9 'd' bbbb bbbbb
 
-      aaa dddd bbbbbb 20 bbbbbbb bbbbbb 4 bbbbbbb bbbbbb 33 bbbbbbb b8 bbb
-        bbb
-  "echo" b9 "print()" b9 "printf()" b9 "console.log()" bbbb b9 bbb
-  "JavaScript" b9 "Python" b9 "C" b9 "Ruby" bbbb b9 bbb "var" b9 "define" b9
-  "const" b9 "let" bbbb b9 bbb "<!-- -->" b9 "//" b9 "#" b9
-  "/* */" bbbb b9 bbb "SELECT" b9 "INSERT" b9 "DELETE" b9 "UPDATE" bbbb b9 bbb
-  "<p>" b9 "<div>" b9 "<span>" b9 "<h1>" bbbb b9 bbb ".classe" b9 "#classe" b9
-  "@classe" b9 "&classe" bbbb b9 bbb "append()" b9 "add()" b9 "insert()" b9
-  "push()" bbbb b9 bbb "class" b9 "struct" b9 "object" b9
-  "function" bbbb b9 bbb "C" b9 "Java" b9 "Bash" b9 "SQL" bbbb b9 bbb "%" b9
-  "$" b9 "&" b9 "#" bbbb b9 bbb "function" b9 "def" b9 "func" b9
-  "method" bbbb b9 bbb "*" b9 "×" b9 "%" b9 "/" bbbb b9 bbb "pwd" b9 "cd" b9
-  "ls" b9 "dir" bbbb b9 bbb "interface" b9 "class" b9 "struct" b9
-  "implements" bbbb b9 bbb "let" b9 "const" b9 "var" b9 "static" bbbb b9 bbb
-  "Java" b9 "C" b9 "Python" b9 "C++" bbbb b9 bbb "Atribuição" b9
-  "Igualdade estrita (valor e tipo)" b9 "Diferença" b9
-  "Incremento" bbbb b9 bbb "SELECT" b9 "UPDATE" b9 "INSERT" b9
-  "DELETE" bbbb b9 bbb "href" b9 "src" b9 "link" b9 "alt" bbbb b9 bbbb bbbbb
+  aaa dddd bbbbbb 20 bbbbbbb bbbbbb 4 bbbbbbb bbbbbb 33 bbbbbbb b8 bbb bbb "echo" b9 "print()" b9 "printf()" b9 "console.log()" bbbb b9 bbb "JavaScript" b9 "Python" b9 "C" b9 "Ruby" bbbb b9 bbb "var" b9 "define" b9 "const" b9 "let" bbbb b9 bbb "<!-- -->" b9 "//" b9 "#" b9 "/* */" bbbb b9 bbb "SELECT" b9 "INSERT" b9 "DELETE" b9 "UPDATE" bbbb b9 bbb "<p>" b9 "<div>" b9 "<span>" b9 "<h1>" bbbb b9 bbb ".classe" b9 "#classe" b9 "@classe" b9 "&classe" bbbb b9 bbb "append()" b9 "add()" b9 "insert()" b9 "push()" bbbb b9 bbb "class" b9 "struct" b9 "object" b9 "function" bbbb b9 bbb "C" b9 "Java" b9 "Bash" b9 "SQL" bbbb b9 bbb "%" b9 "$" b9 "&" b9 "#" bbbb b9 bbb "function" b9 "def" b9 "func" b9 "method" bbbb b9 bbb "*" b9 "×" b9 "%" b9 "/" bbbb b9 bbb "pwd" b9 "cd" b9 "ls" b9 "dir" bbbb b9 bbb "interface" b9 "class" b9 "struct" b9 "implements" bbbb b9 bbb "let" b9 "const" b9 "var" b9 "static" bbbb b9 bbb "Java" b9 "C" b9 "Python" b9 "C++" bbbb b9 bbb "Atribuição" b9 "Igualdade estrita (valor e tipo)" b9 "Diferença" b9 "Incremento" bbbb b9 bbb "SELECT" b9 "UPDATE" b9 "INSERT" b9 "DELETE" bbbb b9 bbb "href" b9 "src" b9 "link" b9 "alt" bbbb b9 bbbb bbbbb
 
-    aaa ddddd bbbbbb 20 bbbbbbb bbbbbb 2 bbbbbbb b8 bbb bbb
-  "b" bbbb b9 bbb "c" bbbb b9 bbb "a" bbbb b9 bbb "b" bbbb b9 bbb
-  "a" bbbb b9 bbb "a" bbbb b9 bbb "a" bbbb b9 bbb "a" bbbb b9 bbb
-  "a" bbbb b9 bbb "b" bbbb b9 bbb "b" bbbb b9 bbb "a" bbbb b9 bbb
-  "a" bbbb b9 bbb "a" bbbb b9 bbb "a" bbbb b9 bbb "b" bbbb b9 bbb
-  "c" bbbb b9 bbb "b" bbbb b9 bbb "b" bbbb b9 bbb "a" bbbb bbbb bbbbb
+  aaa ddddd bbbbbb 20 bbbbbbb bbbbbb 2 bbbbbbb b8 bbb bbb "b" bbbb b9 bbb "c" bbbb b9 bbb "a" bbbb b9 bbb "b" bbbb b9 bbb "a" bbbb b9 bbb "a" bbbb b9 bbb "a" bbbb b9 bbb "a" bbbb b9 bbb "a" bbbb b9 bbb "b" bbbb b9 bbb "b" bbbb b9 bbb "a" bbbb b9 bbb "a" bbbb b9 bbb "a" bbbb b9 bbb "a" bbbb b9 bbb "b" bbbb b9 bbb "c" bbbb b9 bbb "b" bbbb b9 bbb "b" bbbb b9 bbb "a" bbbb bbbb bbbbb
 
-    a d8 b8 d100 bbbbb a d9 b8 d100 bbbbb a d1000 b8 d1m bbbbb
+a d8 b8 d100 bbbbb
+a d9 b8 d100 bbbbb
+a d1000 b8 d1m bbbbb
 
-      aa dddddd b ddddddd bb bbb ccccccc b d1000 eeee d10 bb bbb aaa d00
-        bbbbb c b "%d. %s\n" b9 d1000,
-  ddd bbbbbb d8 bbbbbbb bb bbbbb ccc b a d0m b8 d100 bbbbb d0m e d11 bbbbb d0m
-    ee bb bbb c b
-  "%c. %s\n" b9 dddddddd bbbbbb d0m bbbbbbb b9 dddd bbbbbb d9 bbbbbbb bbbbbb
-    d0m bbbbbbb bb bbbbb bbbb c b "Sua resposta: " bb bbbbb cccc b
-  "%s" b9 eee d00 bb bbbbb ccccc b d00 bz ddddd bbbbbb d8 bbbbbbb bbbbbb d100
-    bbbbbbb bb bbb c b "Correto!\n" bb bbbbb bbbb cccccc bbb c b
-  "Incorreto!\n" bb bbbbb bbbb c b
-  "\n" bb bbbbb d8 ee bbbbb d9 ee bbbbb d1000 ee bbbbb bbbb c b
-  "Fim!\n" bb bbbbb bbbb
+aa dddddd b ddddddd bb bbb
+  ccccccc b d1000 eeee d10 bb bbb
+  aaa d00 bbbbb
+  c b "%d. %s\n"b9 d1000, ddd bbbbbb d8 bbbbbbb bb bbbbb
+  ccc b a d0m b8 d100 bbbbb d0m e d11 bbbbb d0m ee bb bbb 
+    c b "%c. %s\n" b9 dddddddd bbbbbb d0m bbbbbbb b9 dddd bbbbbb d9 bbbbbbb bbbbbb d0m bbbbbbb bb bbbbb
+bbbb
+    c b "Sua resposta: " bb bbbbb
+    cccc b "%s" b9 eee d00 bb bbbbb
+    ccccc b d00 bz ddddd bbbbbb d8 bbbbbbb bbbbbb d100 bbbbbbb bb bbb 
+  c b "Correto!\n" bb bbbbb
+bbbb cccccc bbb 
+  c b "Incorreto!\n" bb bbbbb
+bbbb
+  c b "\n" bb bbbbb
+  d8 ee bbbbb
+  d9 ee bbbbb
+  d1000 ee bbbbb
+bbbb
+c b "Fim!\n" bb bbbbb
+bbbb
 
-    aa dd b bb bbb dddddd b d8 bb bbbbb
-      /* Vou adicionar coisas futuramente aq */
-      bbbb
+aa dd b  bb bbb
+  dddddd b d8 bb bbbbb
+  /* Vou adicionar coisas futuramente aq */
+bbbb
 
-        a d b bb bbb c b "Olá\n" bb bbbbb c b
-  "Bem vindo ao quiz\n" bb bbbbb dd b bb bbbbb cc d100 bbbbb bbbb
+a d b  bb bbb
+  c b "Olá\n" bb bbbbb
+  c b "Bem vindo ao quiz\n" bb bbbbb
+  c b "\n" bb bbbbb
+  dd b bb bbbbb
+  cc d100 bbbbb
+bbbb
